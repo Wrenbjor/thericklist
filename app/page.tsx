6f8ab542566d6d95fc297b58be0d3ad933810173
Wayne <wrenbjor@gmail.com>
2023-11-16 15:32:45 -0500

    fixed display bug

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,19 @@
 import React, { useState, useEffect } from 'react';
 import Sidebar from '../components/Sidebar';
 import MainDisplayGrid from '../components/MainDisplayGrid';
-import { RMCharacter, RMEpisode, AllEpisodes, AllCharacters } from '../types/types';
+import { RMCharacter, RMEpisode } from '../types/types';
 
 const Page = () => {
   const [episodes, setEpisodes] = useState<RMEpisode[]>([]);
   const [selectedEpisode, setSelectedEpisode] = useState<number | null>(null);
   const [selectedEpisodeName, setSelectedEpisodeName] = useState<string | null>(null);
-  const [currentCharacterPage, setCurrentCharacterPage] = useState<number>(1);
   const [characters, setCharacters] = useState<RMCharacter[]>([]);
-  const [currentPage, setCurrentPage] = useState<number>(1);
+  
   
   const fetchCharacters = async () => {
     const response = await fetch('https://rickandmortyapi.com/api/character');
     const data = await response.json();
-    return data.results;
+    setCharacters(data.results); // Set the characters state directly here.
   };
   
   const fetchCharactersByEpisode = async (episodeId: number): Promise<RMCharacter[]> => {
@@ -32,22 +31,9 @@ const Page = () => {
     return characters.filter((character) => character.episode.includes(episodeData.url));
   };
 
-  const fetchCharactersByPage = async (page: number): Promise<AllCharacters> => {
-    const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`);
-    const data = await response.json();
-    return data;
-  };
-  
-  const handleCharacterPageChange = (page: number) => {
-    setCurrentCharacterPage(page);
-  };
-
   const handleEpisodeSelect = async (id: number) => {
     if (selectedEpisode === id) {
-      // If the selected episode is the same as the currently selected episode,
-      // fetch all characters and set them as the current characters.
       const allCharacters = await fetchCharacters();
-      setCharacters(allCharacters);
       setSelectedEpisode(null);
       setSelectedEpisodeName(null);
     } else {
@@ -63,7 +49,6 @@ const Page = () => {
 
   const fetchAllEpisodes = async () => {
     try {
-      // First, fetch the first page to get the total number of pages
       let response = await fetch('https://rickandmortyapi.com/api/episode');
       let data = await response.json();
       let episodes = data.results;
@@ -79,39 +64,14 @@ const Page = () => {
       setEpisodes(episodes);
     } catch (error) {
       console.error('Error fetching all episodes:', error);
-      // Handle error as needed
     }
   };
 
-  const fetchAllCharacters = async (): Promise<RMCharacter[]> => {
-    try {
-      // First, fetch the first page to get the total number of pages
-      let response = await fetch('https://rickandmortyapi.com/api/character');
-      let data = await response.json();
-      let allCharacters = data.results;
-      const totalPages = data.info.pages;
-  
-      // Fetch characters from remaining pages
-      for (let page = 2; page <= totalPages; page++) {
-        response = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`);
-        data = await response.json();
-        allCharacters = allCharacters.concat(data.results);
-      }
-  
-      return allCharacters;
-    } catch (error) {
-      console.error('Error fetching all characters:', error);
-      return []; // Return an empty array or handle the error as needed
-    }
-  };
-  
   
   useEffect(() => {
     fetchAllEpisodes();
-    fetchCharactersByPage(currentCharacterPage).then(data => {
-      setCharacters(data.results);
-    });
-  }, [currentPage, currentCharacterPage]);
+    fetchCharacters();
+  }, []);
 
   return (
     <div className="flex">
